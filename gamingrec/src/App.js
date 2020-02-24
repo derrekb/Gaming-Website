926c7bf67750267e913c9fbdee3deca95997afec
derrekb <derrekbeeck@gmail.com>
2020-02-24 17:18:46 -0500

    yo brochacho here are the changes

--- gamingrec/src/App.js
+++ gamingrec/src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
-import './App.css';
+//components
+import Header from './headerComponent/header';
+import Footer from './headerComponent/footerComponent/footer';
+import HomePage from './headerComponent/pages/homePage';
+import MyPlayedGames from './headerComponent/pages/MyPlayedGames';
+
+import './scss/default.scss';
+  
+
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
+       <Header/>
+
+       <Route exact path='/' component={HomePage} />
+       <Route exact path='/MyPlayedGames' component={MyPlayedGames} />
+
+      
     </div>
+    </Router>
   );
 }
 
---
+++ gamingrec/src/headerComponent/pages/homePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function HomePage() {
+  return (
+    <div className="container-fluid">
+<h1>
+  Home Page Content
+</h1>
+<p>
+
+</p>
+    </div>
+  );
+}
+
+export default HomePage;
+
