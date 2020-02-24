f04fd65171c43961ddb9f9c900a3aa7ddef65faf
Hans Joshua D. Yabao <43555169+yabaohan@users.noreply.github.com>
2020-02-24 16:20:06 -0500

    Here are the changes...

--- gamingrec/src/App.js
+++ gamingrec/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reeeeeeeeeeeeeeeeee.
         </p>
         <a
           className="App-link"
